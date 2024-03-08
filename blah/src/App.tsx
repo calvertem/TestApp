782cb83386ce1025d40d6511fdf3b2bda7620cb0
calvertem <calvertemma25@gmail.com>
2024-03-08 14:44:40 -0600

    final stuff

--- blah/src/App.tsx
+++ blah/src/App.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return (
+    <div>
+      <h1>March Madness!</h1>
+      <h3>NCAA Tourney Time!!!</h3>
+      <p>
+        We wanted to make sure everyone had all the information they needed this
+        tournament season. So here is all you need to know about all the teams
+        playing in the tournament this year.
+      </p>
+    </div>
+  );
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}{' '}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {data.teams.map((oneTeam) => (
+        <Team key={oneTeam.tid} {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
